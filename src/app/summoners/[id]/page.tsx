bbe7842ec21fffe42a328390544312ef1b0c1c04
Haru-Lee <vr1997@naver.com>
2024-04-17 14:27:19 +0900

    Update page.tsx

--- src/app/summoners/[id]/page.tsx
+++ src/app/summoners/[id]/page.tsx
@@ -1,9 +1,7 @@
-const summoners = (props) => {
-  const {id} = props.params
-  console.log('props',id)
+const summoners = () => {
   return (
     <div className="max-w-7xl m-[auto]">
-      <h1 className='text-white'>소환사id: {id}</h1>
+      <h1 className='text-white'>소환사id</h1>
     </div>
   )
 }
