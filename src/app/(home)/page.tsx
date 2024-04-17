b75438a139fcd869dd14550fbf3b21b43f471b64
Haru-Lee <vr1997@naver.com>
2024-04-17 14:23:43 +0900

    Update page.tsx

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -9,7 +9,7 @@ const Home = () => {
        <div className="flex flex-row w-[560px] h-[60px] gap-4 rounded py-2 pl-2 pr-[21px] bg-white">
          <div className="flex flex-row items-center gap-[20px] bg-gray-50 pl-[14px] py-[13px] pr-[12px]">
            <div className="text-[14px] font-semibold">KR</div>
-           <div className="rotate-90">></div>
+           <div className="rotate-90">{'>'}</div>
          </div>
          <input className=" w-full enabled:focus:outline-none" />
          <Image src={searchIco} alt="검색" width={22} height={22} />
